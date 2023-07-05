fdd73dfe66c3d1f2a054fddb8e60825cb775b238
Egidijus Luinevicius <egidijus.lu@protonmail.com>
2023-07-05 16:31:31 +0100

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonsComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
